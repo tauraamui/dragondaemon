f96cc22576d0f22b581d3557df5806a2e5a53f32
tauraamui <adamplewis@pm.me>
2020-12-13 21:46:15 +0000

    Move connection and server related code to own two files

--- media/connection.go
+++ media/connection.go
@@ -1,9 +1,14 @@
 package media
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"sync"
 	"sync/atomic"
+	"time"
 
+	"github.com/tacusci/logging"
 	"gocv.io/x/gocv"
 )
 
@@ -57,10 +62,70 @@ func (c *Connection) Title() string {
 	return c.title
 }
 
+func (c *Connection) PersistToDisk() {
+	img := c.FetchFrame()
+	defer img.Close()
+	outputFile := fetchClipFilePath(c.persistLocation, c.title)
+	writer, err := gocv.VideoWriterFile(outputFile, "mp4v", 30, img.Cols(), img.Rows(), true)
+
+	if err != nil {
+		logging.Error(fmt.Sprintf("Opening video writer device: %v\n", err))
+	}
+	defer writer.Close()
+
+	var framesWritten uint
+	for framesWritten = 0; framesWritten < 30*uint(c.secondsPerClip); framesWritten++ {
+		if ok := c.vc.Read(img); !ok {
+			logging.Error(fmt.Sprintf("Device for stream at [%s] closed", c.title))
+			return
+		}
+
+		if img.Empty() {
+			logging.Debug("Skipping frame...")
+			continue
+		}
+
+		if err := writer.Write(*img); err != nil {
+			logging.Error(fmt.Sprintf("Unable to write frame to file: %v", err))
+		}
+	}
+}
+
+func (c *Connection) FetchFrame() *gocv.Mat {
+	img := gocv.NewMat()
+	c.vc.Read(&img)
+	return &img
+}
+
 func (c *Connection) Close() error {
 	atomic.StoreInt32(&c.inShutdown, 1)
 	if c.window != nil {
 		c.window.Close()
 	}
 	return c.vc.Close()
+}
+
+func fetchClipFilePath(rootDir string, clipsDir string) string {
+	if len(rootDir) > 0 {
+		ensureDirectoryExists(rootDir)
+	} else {
+		rootDir = "."
+	}
+
+	todaysDate := time.Now().Format("2006-01-02")
+
+	if len(clipsDir) > 0 {
+		ensureDirectoryExists(fmt.Sprintf("%s/%s/%s", rootDir, clipsDir, todaysDate))
+	}
+
+	return filepath.FromSlash(fmt.Sprintf("%s/%s/%s/%s.mp4", rootDir, clipsDir, todaysDate, time.Now().Format("2006-01-02 15.04.05")))
+}
+
+func ensureDirectoryExists(path string) error {
+	err := os.Mkdir(path, os.ModePerm)
+
+	if err == nil || os.IsExist(err) {
+		return nil
+	}
+	return err
 }
--- media/server.go
+++ media/server.go
@@ -1,7 +1,12 @@
 package media
 
 import (
+	"fmt"
 	"sync"
+	"sync/atomic"
+
+	"github.com/tacusci/logging"
+	"gocv.io/x/gocv"
 )
 
 // Server manages receiving RTSP streams and persisting clips to disk
@@ -18,4 +23,80 @@ func NewServer() *Server {
 
 func (s *Server) IsRunning() bool {
 	return !s.shuttingDown()
+}
+
+func (s *Server) Connect(
+	title string,
+	rtspStream string,
+	persistLocation string,
+	secondsPerClip int,
+) {
+	vc, err := gocv.OpenVideoCapture(rtspStream)
+	if err != nil {
+		logging.Error(fmt.Sprintf("Unable to connect to stream at [%s]: %v", rtspStream, err))
+	}
+
+	logging.Info(fmt.Sprintf("Connected to stream at [%s]", rtspStream))
+	conn := NewConnection(
+		title,
+		persistLocation,
+		secondsPerClip,
+		vc,
+	)
+	s.trackConnection(conn, true)
+}
+
+func (s *Server) ActiveConnections() []*Connection {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	connections := make([]*Connection, 0, len(s.connections))
+	for k := range s.connections {
+		connections = append(connections, k)
+	}
+
+	return connections
+}
+
+func (s *Server) trackConnection(conn *Connection, add bool) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if s.connections == nil {
+		s.connections = make(map[*Connection]struct{})
+	}
+
+	if add {
+		if s.shuttingDown() {
+			return false
+		}
+		s.connections[conn] = struct{}{}
+	} else {
+		delete(s.connections, conn)
+	}
+	return true
+}
+
+func (s *Server) closeConnectionsLocked() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	var err error
+	for conn := range s.connections {
+		if cerr := (*conn).Close(); cerr != nil && err == nil {
+			err = cerr
+		}
+		delete(s.connections, conn)
+	}
+	return err
+}
+
+func (s *Server) shuttingDown() bool {
+	return atomic.LoadInt32(&s.inShutdown) != 0
+}
+
+func (s *Server) Shutdown() {
+	atomic.StoreInt32(&s.inShutdown, 1)
+}
+
+func (s *Server) Close() error {
+	return s.closeConnectionsLocked()
 }
