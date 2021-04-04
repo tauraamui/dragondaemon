e7c1fddcf8a9e93f7a43c94f52e1b9eb99d67de7
tauraamui <adamplewis@pm.me>
2021-04-04 14:12:29 +0100

    Add auto gen uuid for user table and encrypt given plain password in DB

--- database/models/user.go
+++ database/models/user.go
@@ -1,13 +1,48 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/tacusci/logging/v2"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 func init() {
-	addToCollection(User{})
+	addToCollection(&User{})
 }
 
 type User struct {
 	gorm.Model
+	UUID     string
 	Name     string
 	AuthHash string
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	u.UUID = uuid.NewString()
+	u.AuthHash = enc(u.AuthHash)
+	return nil
+}
+
+func enc(p string) string {
+	h, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.DefaultCost)
+	if err != nil {
+		logging.Error(fmt.Errorf("unable to generate hash and salt from password: %w", err).Error())
+		return p
+	}
+
+	return string(h)
+}
+
+func cmp(h, p string) bool {
+	hb, pb := []byte(h), []byte(p)
+	err := bcrypt.CompareHashAndPassword(hb, pb)
+	if err != nil {
+		logging.Error(fmt.Errorf("comparing hashed password to plain password failed: %w", err).Error())
+		return false
+	}
+
+	return true
 }
