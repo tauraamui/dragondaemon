1b72299f7d7d0c8d56df7552b741daafb3f6d0ab
tauraamui <adamplewis@pm.me>
2021-06-02 22:28:39 +0100

    Test setup returns error on attempt to resolve db file path

--- pkg/database/db_test.go
+++ pkg/database/db_test.go
@@ -1,11 +1,21 @@
 package data_test
 
 import (
+	"errors"
+
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 	data "github.com/tauraamui/dragondaemon/pkg/database"
 )
 
 var _ = Describe("Data", func() {
-	data.Setup()
+	It("Should return error from setup due to path resolution failure", func() {
+		data.OverloadUC(func() (string, error) {
+			return "", errors.New("test cache dir error")
+		})
+		err := data.Setup()
+
+		Expect(err).ToNot(BeNil())
+		Expect(err.Error()).To(Equal("unable to resolve dd.db database file location: test cache dir error"))
+	})
 })
