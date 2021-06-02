42746ee8b9c670fd07d063d661966c17c9ff3253
tauraamui <adamplewis@pm.me>
2021-06-02 22:03:08 +0100

    Define initial tests

---
+++ pkg/database/db_test.go
@@ -0,0 +1,11 @@
+package data_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	// . "github.com/onsi/gomega"
+	data "github.com/tauraamui/dragondaemon/pkg/database"
+)
+
+var _ = Describe("Data", func() {
+	data.Setup()
+})
