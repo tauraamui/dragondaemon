dee518a7e0def924021c4514e7f7c46e87af38b5
tauraamui <adamplewis@pm.me>
2021-06-03 08:02:19 +0100

    Initial test for database setup

--- pkg/database/db_test.go
+++ pkg/database/db_test.go
@@ -2,17 +2,29 @@ package data_test
 
 import (
 	"errors"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/spf13/afero"
 	data "github.com/tauraamui/dragondaemon/pkg/database"
 )
 
 var _ = Describe("Data", func() {
-	// Context("Setup run against blank file system", func() {
-	// 	It("Should create full file path for DB", func() {
-	// 	})
-	// })
+	Context("Setup run against blank file system", func() {
+		It("Should create full file path for DB", func() {
+			resetFS := data.OverloadFS(afero.NewMemMapFs())
+			defer resetFS()
+
+			resetPromptReader := data.OverloadPromptReader(
+				strings.NewReader("testadmin\ntestpassword\n"),
+			)
+			defer resetPromptReader()
+
+			err := data.Setup()
+			Expect(err).ToNot(BeNil())
+		})
+	})
 
 	It("Should return error from setup due to path resolution failure", func() {
 		reset := data.OverloadUC(func() (string, error) {
