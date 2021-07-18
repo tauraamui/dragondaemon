1dba86f3775607f514f3e0bb223fe9a0a27b56f0
tauraamui <adamplewis@pm.me>
2021-07-19 00:38:47 +0100

    Test server connect failure to open video stream connection

--- pkg/media/server_test.go
+++ pkg/media/server_test.go
@@ -1,6 +1,7 @@
 package media
 
 import (
+	"errors"
 	"fmt"
 
 	. "github.com/onsi/ginkgo"
@@ -37,6 +38,47 @@ var _ = Describe("Server", func() {
 				Expect(infoLogs[0]).To(ContainSubstring(
 					fmt.Sprintf(
 						"Connected to stream [%s] at [%s]",
+						"TestConnection", "fake-rtsp-addr",
+					),
+				))
+			})
+
+			It("Should error on connecting to stream and not track a connection", func() {
+				resetOpenVidCapOverload := OverloadOpenVideoCapture(
+					func(string, string, int, bool, string, bool) (VideoCapturable, error) {
+						return nil, errors.New("test fail to open connection")
+					},
+				)
+				defer resetOpenVidCapOverload()
+
+				infoLogs := []string{}
+				resetLogInfo := OverloadLogInfo(func(format string, args ...interface{}) {
+					infoLogs = append(infoLogs, fmt.Sprintf(format, args...))
+				})
+				defer resetLogInfo()
+
+				errorLogs := []string{}
+				resetLogError := OverloadLogError(func(format string, args ...interface{}) {
+					errorLogs = append(errorLogs, fmt.Sprintf(format, args...))
+				})
+				defer resetLogError()
+
+				server.Connect(
+					"TestConnection",
+					"fake-rtsp-addr",
+					ConnectonSettings{
+						MockWriter:      true,
+						MockCapturer:    true,
+						PersistLocation: "/testroot/clips",
+					},
+				)
+
+				Expect(server.connections).To(HaveLen(0))
+				Expect(infoLogs).To(HaveLen(0))
+				Expect(errorLogs).To(HaveLen(1))
+				Expect(errorLogs[0]).To(ContainSubstring(
+					fmt.Sprintf(
+						"Unable to connect to stream [%s] at [%s]",
 						"TestConnection", "fake-rtsp-addr",
 					),
 				))
