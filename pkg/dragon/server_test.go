4e225e92fa902b62341a4cd21e192dcc0998b967
tauraamui <adamplewis@pm.me>
2021-08-03 09:55:42 +0100

    Simplify test value checking by using assertion lib

--- pkg/dragon/server_test.go
+++ pkg/dragon/server_test.go
@@ -1,18 +1,28 @@
 package dragon_test
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/tauraamui/dragondaemon/pkg/configdef"
 	"github.com/tauraamui/dragondaemon/pkg/dragon"
 )
 
 type testConfigResolver struct {
 	resolveCallback func()
+	resolveError    error
 }
 
 func (tcc testConfigResolver) Resolve() (configdef.Values, error) {
-	tcc.resolveCallback()
+	if tcc.resolveCallback != nil {
+		tcc.resolveCallback()
+	}
+
+	if tcc.resolveError != nil {
+		return configdef.Values{}, tcc.resolveError
+	}
+
 	return configdef.Values{
 		Cameras: []configdef.Camera{{Title: "Test camera"}},
 	}, nil
@@ -20,9 +30,7 @@ func (tcc testConfigResolver) Resolve() (configdef.Values, error) {
 
 func TestNewServer(t *testing.T) {
 	s := dragon.NewServer(testConfigResolver{})
-	if s == nil {
-		t.Error("New server's response cannot be nil pointer")
-	}
+	assert.NotNil(t, s, "new server's response cannot be nil pointer")
 }
 
 func TestServerLoadConfig(t *testing.T) {
@@ -33,11 +41,15 @@ func TestServerLoadConfig(t *testing.T) {
 	s := dragon.NewServer(testConfigResolver{resolveCallback: cb})
 	err := s.LoadConfiguration()
 
-	if err != nil {
-		t.Error("Server load config returned error despite that being impossible")
-	}
+	assert.Nil(t, err, "Resolve returned error when this should be impossible")
+	assert.True(t, configResolved)
+}
 
-	if !configResolved {
-		t.Error("Server load config does not call resolve against config resolver")
-	}
+func TestServerLoadConfigGivesErrorOnResolveError(t *testing.T) {
+	s := dragon.NewServer(testConfigResolver{
+		resolveError: errors.New("test unable to resolve config"),
+	})
+	err := s.LoadConfiguration()
+	assert.NotNil(t, err)
+	assert.EqualError(t, err, "test unable to resolve config")
 }
