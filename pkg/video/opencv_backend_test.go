ff5197a676bdc2ec654ce13c86cb392d03f9b924
tauraamui <adamplewis@pm.me>
2021-08-05 02:30:42 +0100

    Test open video capture is called by "connecting" to local video stream

--- pkg/video/opencv_backend_test.go
+++ pkg/video/opencv_backend_test.go
@@ -3,17 +3,43 @@ package video
 import (
 	"context"
 	"errors"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gocv.io/x/gocv"
 )
 
+func overloadOpenVidCap(overload func(addr string) (*gocv.VideoCapture, error)) func() {
+	openVidCapRef := openVideoCapture
+	openVideoCapture = overload
+	return func() { openVideoCapture = openVidCapRef }
+}
+
 func TestOpenVideoStreamInvokesOpenVideoCapture(t *testing.T) {
-	openVideoCapture = func(addr string) (*gocv.VideoCapture, error) {
-		return nil, errors.New("test connect error")
-	}
+	resetOpenVidCap := overloadOpenVidCap(
+		func(addr string) (*gocv.VideoCapture, error) {
+			return nil, errors.New("test connect error")
+		},
+	)
+	defer resetOpenVidCap()
+
 	conn := openCVConnection{}
 	err := conn.connect(context.TODO(), "TestAddr")
 	assert.EqualError(t, err, "test connect error")
+}
+
+func TestOpenVideoStream(t *testing.T) {
+	mp4Dir := os.TempDir()
+	err := RestoreAsset(mp4Dir, "small.mp4")
+	require.NoError(t, err)
+
+	mp4FilePath := filepath.Join(mp4Dir, "small.mp4")
+	defer os.Remove(mp4FilePath)
+
+	conn := openCVConnection{}
+	err = conn.connect(context.TODO(), mp4FilePath)
+	require.NoError(t, err)
 }
