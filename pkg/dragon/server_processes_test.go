5e26f0fab026300d6a0c36ffbe95848f9bd1edcf
tauraamui <adamplewis@pm.me>
2021-08-07 01:04:51 +0100

    Sub divide new server processes test into test suite structure

--- pkg/dragon/server_processes_test.go
+++ pkg/dragon/server_processes_test.go
@@ -5,28 +5,68 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"github.com/tauraamui/dragondaemon/internal/videotest"
 	"github.com/tauraamui/dragondaemon/pkg/configdef"
 	"github.com/tauraamui/dragondaemon/pkg/dragon"
 	"github.com/tauraamui/dragondaemon/pkg/video"
 )
 
-func TestServerRunProcesses(t *testing.T) {
-	mp4FilePath, err := videotest.RestoreMp4File()
-	require.NoError(t, err)
-	defer func() { os.Remove(mp4FilePath) }()
+type ServerProcessTestSuite struct {
+	suite.Suite
+	mp4FilePath string
+	server      dragon.Server
+}
 
-	s := dragon.NewServer(testConfigResolver{
+func (suite *ServerProcessTestSuite) SetupSuite() {
+	mp4FilePath, err := videotest.RestoreMp4File()
+	require.NoError(suite.T(), err)
+	suite.mp4FilePath = mp4FilePath
+}
+
+func (suite *ServerProcessTestSuite) TeardownSuite() {
+	require.NoError(suite.T(), os.Remove(suite.mp4FilePath))
+}
+
+func (suite *ServerProcessTestSuite) SetupTest() {
+	suite.server = dragon.NewServer(testConfigResolver{
 		resolveConfigs: func() configdef.Values {
 			return configdef.Values{
 				Cameras: []configdef.Camera{
-					{Title: "TestConn", Address: mp4FilePath},
+					{Title: "TestConn", Address: suite.mp4FilePath},
 				},
 			}
 		},
 	}, video.DefaultBackend())
-	require.NoError(t, s.LoadConfiguration())
-	require.Len(t, s.Connect(), 0)
-	s.RunProcesses()
-	<-s.Shutdown()
 }
+
+func (suite *ServerProcessTestSuite) TestRunProcesses() {
+	require.NoError(suite.T(), suite.server.LoadConfiguration())
+	require.Len(suite.T(), suite.server.Connect(), 0)
+	suite.server.RunProcesses()
+	<-suite.server.Shutdown()
+}
+
+func TestServerProcessTestSuite(t *testing.T) {
+	suite.Run(t, &ServerProcessTestSuite{})
+}
+
+// func TestServerRunProcesses(t *testing.T) {
+// 	mp4FilePath, err := videotest.RestoreMp4File()
+// 	require.NoError(t, err)
+// 	defer func() { os.Remove(mp4FilePath) }()
+
+// 	s := dragon.NewServer(testConfigResolver{
+// 		resolveConfigs: func() configdef.Values {
+// 			return configdef.Values{
+// 				Cameras: []configdef.Camera{
+// 					{Title: "TestConn", Address: mp4FilePath},
+// 				},
+// 			}
+// 		},
+// 	}, video.DefaultBackend())
+// 	require.NoError(t, s.LoadConfiguration())
+// 	require.Len(t, s.Connect(), 0)
+// 	s.RunProcesses()
+// 	<-s.Shutdown()
+// }
