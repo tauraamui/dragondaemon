db2e01955352530d74358c480b4e1509defc2bb5
tauraamui <adamplewis@pm.me>
2021-08-18 16:18:29 +0100

    Setup test by creating initial config file with content

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -5,17 +5,25 @@ import (
 	"testing"
 
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
 type ConfigTestSuite struct {
 	suite.Suite
-	fs afero.Fs
+	fs          afero.Fs
+	fileContent string
 }
 
 func (suite *ConfigTestSuite) SetupSuite() {
 	suite.fs = afero.NewMemMapFs()
+	suite.fileContent = `
+		{
+			"max_clip_age_in_days": 30,
+			"cameras": []
+		}
+	`
 }
 
 func (suite *ConfigTestSuite) TearDownSuite() {
@@ -23,9 +31,16 @@ func (suite *ConfigTestSuite) TearDownSuite() {
 }
 
 func (suite *ConfigTestSuite) TestLoadConfig() {
-	dir, err := resolveConfigPath()
+	path, err := resolveConfigPath()
 	require.NoError(suite.T(), err)
-	require.NoError(suite.T(), suite.fs.Mkdir(dir, os.ModeDir|os.ModePerm))
+	require.NoError(suite.T(), suite.fs.MkdirAll(path, os.ModeDir|os.ModePerm))
+
+	configFile, err := suite.fs.Create(path)
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), configFile)
+
+	_, err = configFile.WriteString(suite.fileContent)
+	assert.NoError(suite.T(), err)
 }
 
 func TestConfigTestSuite(t *testing.T) {
