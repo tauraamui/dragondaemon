f358a014255985c8f691735c82e2c9a8eeab5ce0
tauraamui <adamplewis@pm.me>
2021-08-19 11:01:32 +0100

    Define test for loading test created config file

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,24 +8,21 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"github.com/tauraamui/dragondaemon/pkg/configdef"
 )
 
 type ConfigTestSuite struct {
 	suite.Suite
-	fs          afero.Fs
-	path        string
-	configFile  afero.File
-	fileContent string
+	fs         afero.Fs
+	path       string
+	configFile afero.File
 }
 
 func (suite *ConfigTestSuite) SetupSuite() {
 	suite.fs = afero.NewMemMapFs()
-	suite.fileContent = `
-		{
-			"max_clip_age_in_days": 30,
-			"cameras": []
-		}
-	`
+
+	// use in memory FS in implementation for tests
+	fs = suite.fs
 }
 
 func (suite *ConfigTestSuite) TearDownSuite() {
@@ -44,7 +41,24 @@ func (suite *ConfigTestSuite) SetupTest() {
 
 	suite.configFile = configFile
 
-	_, err = configFile.WriteString(suite.fileContent)
+	// can be overridden this so reset it back before
+	// each test to ensure that it's an opt in thing per
+	// individual test
+	suite.overwriteTestConfig(
+		`{
+			"debug": true,
+			"secret": "DJIF3fje943fi4jefgo0",
+			"max_clip_age_in_days": 19,
+			"cameras": []
+		}`,
+	)
+}
+
+func (suite *ConfigTestSuite) overwriteTestConfig(config string) {
+	require.NoError(suite.T(), suite.configFile.Truncate(0))
+	_, err := suite.configFile.Seek(0, 0)
+	require.NoError(suite.T(), err)
+	_, err = suite.configFile.WriteString(config)
 	assert.NoError(suite.T(), err)
 }
 
@@ -54,7 +68,24 @@ func (suite *ConfigTestSuite) TearDownTest() {
 }
 
 func (suite *ConfigTestSuite) TestLoadConfig() {
+	config, err := Load()
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), config)
 
+	assert.Equal(suite.T(), true, config.Debug)
+	assert.Equal(suite.T(), "DJIF3fje943fi4jefgo0", config.Secret)
+	assert.Equal(suite.T(), 19, config.MaxClipAgeInDays)
+	assert.ElementsMatch(suite.T(), config.Cameras, []configdef.Camera{})
+}
+
+func (suite *ConfigTestSuite) TestLoadConfigWithCameras() {
+	suite.overwriteTestConfig(
+		`{}`,
+	)
+
+	config, err := Load()
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), config)
 }
 
 func TestConfigTestSuite(t *testing.T) {
