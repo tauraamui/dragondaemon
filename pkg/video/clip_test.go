6a9e6b404f49a07c6f2a84087fa58a0a89937e6c
tauraamui <adamplewis@pm.me>
2021-08-24 20:05:31 +0100

    Test clip tracks frames and then closes them on close

--- pkg/video/clip_test.go
+++ pkg/video/clip_test.go
@@ -4,9 +4,52 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+const testClipPath = "/testroot/clips/TestConn/2010-02-2/2010-02-02 19:45:00"
+
 func TestNewClip(t *testing.T) {
-	clip := NewClip("/testroot/clips/TestConn/2010-02-2/2010-02-02 19:45:00")
+	clip := NewClip(testClipPath)
 	assert.NotNil(t, clip)
+}
+
+type testFrame struct {
+	onClose func()
+}
+
+func (frame *testFrame) DataRef() interface{} {
+	return nil
+}
+
+func (frame *testFrame) Close() {
+	if frame.onClose != nil {
+		frame.onClose()
+	}
+}
+
+func TestClipAppendFrameTracksFrameButDoesNotCloseIt(t *testing.T) {
+	clip := NewClip(testClipPath)
+	require.NotNil(t, clip)
+
+	frameCloseInvoked := false
+	frame := &testFrame{onClose: func() { frameCloseInvoked = true }}
+	clip.AppendFrame(frame)
+
+	assert.Contains(t, clip.frames, frame)
+	assert.False(t, frameCloseInvoked)
+}
+
+func TestClipAppendFrameTracksFrameWhichIsThenClosed(t *testing.T) {
+	clip := NewClip(testClipPath)
+	require.NotNil(t, clip)
+
+	frameCloseInvoked := false
+	frame := &testFrame{onClose: func() { frameCloseInvoked = true }}
+	clip.AppendFrame(frame)
+
+	assert.Contains(t, clip.frames, frame)
+	clip.Close()
+
+	assert.True(t, frameCloseInvoked)
 }
