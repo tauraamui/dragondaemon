36a566981ac07afe88af905fbb77cefcb18c4201
tauraamui <adamplewis@pm.me>
2021-08-24 20:19:00 +0100

    Test appending frame to closed clip crashes out whole program

--- pkg/video/clip_test.go
+++ pkg/video/clip_test.go
@@ -1,13 +1,22 @@
 package video
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/tacusci/logging/v2"
+	"github.com/tauraamui/dragondaemon/pkg/log"
 )
 
 const testClipPath = "/testroot/clips/TestConn/2010-02-2/2010-02-02 19:45:00"
+
+func overloadFatalLog(overload func(string, ...interface{})) func() {
+	logFatalRef := log.Fatal
+	log.Fatal = overload
+	return func() { log.Fatal = logFatalRef }
+}
 
 func TestNewClip(t *testing.T) {
 	clip := NewClip(testClipPath)
@@ -52,4 +61,25 @@ func TestClipAppendFrameTracksFrameWhichIsThenClosed(t *testing.T) {
 	clip.Close()
 
 	assert.True(t, frameCloseInvoked)
+}
+
+func TestClipAppendFailsIfClipAlreadyClosed(t *testing.T) {
+	var fatalLogs []string
+	resetLogFatal := overloadFatalLog(
+		func(format string, a ...interface{}) {
+			fatalLogs = append(fatalLogs, fmt.Sprintf(format, a...))
+		},
+	)
+	defer resetLogFatal()
+
+	logging.CurrentLoggingLevel = logging.SilentLevel
+	defer func() { logging.CurrentLoggingLevel = logging.WarnLevel }()
+
+	clip := NewClip(testClipPath)
+	require.NotNil(t, clip)
+
+	clip.Close()
+	clip.AppendFrame(&testFrame{})
+
+	assert.Contains(t, fatalLogs, "cannot append frame to closed clip")
 }
