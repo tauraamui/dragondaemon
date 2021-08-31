39b2e1b3bd83cc4b2116c705abd7da32ef852367
tauraamui <adamplewis@pm.me>
2021-08-31 22:59:46 +0100

    Define explicit process for streaming a camera connection

--- pkg/dragon/process/core.go
+++ pkg/dragon/process/core.go
@@ -1,9 +1,6 @@
 package process
 
 import (
-	"fmt"
-	"sync"
-
 	"github.com/spf13/afero"
 	"github.com/tauraamui/dragondaemon/pkg/camera"
 	"github.com/tauraamui/dragondaemon/pkg/video"
@@ -32,65 +29,63 @@ type persistCameraToDisk struct {
 }
 
 func (proc *persistCameraToDisk) Setup() {
-	writeClipsToDiskProcess := Settings{
-		WaitForShutdownMsg: fmt.Sprintf("Stopping writing clips to disk from [%s] video stream...", proc.cam.Title()),
-		Process:            WriteClipsToDiskProcess(proc.clips, proc.writer),
-	}
-	proc.writeClips = New(writeClipsToDiskProcess)
+	proc.streamProcess = NewStreamConnProcess(proc.cam, proc.frames)
+	// writeClipsToDiskProcess := Settings{
+	// 	WaitForShutdownMsg: fmt.Sprintf("Stopping writing clips to disk from [%s] video stream...", proc.cam.Title()),
+	// 	Process:            WriteClipsToDiskProcess(proc.clips, proc.writer),
+	// }
+	// proc.writeClips = New(writeClipsToDiskProcess)
 
-	generateClipsFromFramesProcess := Settings{
-		WaitForShutdownMsg: fmt.Sprintf("Stopping generating clips from [%s] video stream...", proc.cam.Title()),
-		Process: GenerateClipsProcess(
-			proc.frames, proc.clips, proc.cam.FullPersistLocation(), proc.cam.FPS(), proc.cam.SPC(),
-		),
-	}
-	proc.generateClips = New(generateClipsFromFramesProcess)
+	// generateClipsFromFramesProcess := Settings{
+	// 	WaitForShutdownMsg: fmt.Sprintf("Stopping generating clips from [%s] video stream...", proc.cam.Title()),
+	// 	Process: GenerateClipsProcess(
+	// 		proc.frames, proc.clips, proc.cam.FullPersistLocation(), proc.cam.FPS(), proc.cam.SPC(),
+	// 	),
+	// }
+	// proc.generateClips = New(generateClipsFromFramesProcess)
 
-	streamProcess := Settings{
-		WaitForShutdownMsg: fmt.Sprintf("Closing camera [%s] video stream...", proc.cam.Title()),
-		Process:            StreamProcess(proc.cam, proc.frames),
-	}
-	proc.streamProcess = New(streamProcess)
+	// streamProcess := Settings{
+	// 	WaitForShutdownMsg: fmt.Sprintf("Closing camera [%s] video stream...", proc.cam.Title()),
+	// 	Process:            StreamProcess(proc.cam, proc.frames),
+	// }
+	// proc.streamProcess = New(streamProcess)
 
-	deleteProcess := Settings{
-		WaitForShutdownMsg: fmt.Sprintf("Stopping deleting old saved clips for [%s]", proc.cam.Title()),
-		Process:            DeleteOldClips(proc.cam),
-	}
-	proc.deleteClips = New(deleteProcess)
+	// deleteProcess := Settings{
+	// 	WaitForShutdownMsg: fmt.Sprintf("Stopping deleting old saved clips for [%s]", proc.cam.Title()),
+	// 	Process:            DeleteOldClips(proc.cam),
+	// }
+	// proc.deleteClips = New(deleteProcess)
 }
 
 func (proc *persistCameraToDisk) Start() {
-	proc.deleteClips.Start()
-	proc.writeClips.Start()
-	proc.generateClips.Start()
 	proc.streamProcess.Start()
+	// proc.deleteClips.Start()
+	// proc.writeClips.Start()
+	// proc.generateClips.Start()
+	// proc.streamProcess.Start()
 }
 
 func (proc *persistCameraToDisk) Stop() {
-	proc.deleteClips.Stop()
-	proc.writeClips.Stop()
-	proc.generateClips.Stop()
+	// proc.deleteClips.Stop()
+	// proc.writeClips.Stop()
+	// proc.generateClips.Stop()
 	proc.streamProcess.Stop()
 }
 
 func (proc *persistCameraToDisk) Wait() {
-	wg := sync.WaitGroup{}
-	wg.Add(4)
-	go func(wg *sync.WaitGroup) {
-		proc.deleteClips.Wait()
-		wg.Done()
-	}(&wg)
-	go func(wg *sync.WaitGroup) {
-		proc.writeClips.Wait()
-		wg.Done()
-	}(&wg)
-	go func(wg *sync.WaitGroup) {
-		proc.generateClips.Wait()
-		wg.Done()
-	}(&wg)
-	go func(wg *sync.WaitGroup) {
-		proc.streamProcess.Wait()
-		wg.Done()
-	}(&wg)
-	wg.Wait()
+	// wg := sync.WaitGroup{}
+	// wg.Add(4)
+	// go func(wg *sync.WaitGroup) {
+	// 	proc.deleteClips.Wait()
+	// 	wg.Done()
+	// }(&wg)
+	// go func(wg *sync.WaitGroup) {
+	// 	proc.writeClips.Wait()
+	// 	wg.Done()
+	// }(&wg)
+	// go func(wg *sync.WaitGroup) {
+	// 	proc.generateClips.Wait()
+	// 	wg.Done()
+	// }(&wg)
+	proc.streamProcess.Wait()
 }
---
+++ pkg/dragon/process/stream_conn_process.go
@@ -0,0 +1,70 @@
+package process
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/tauraamui/dragondaemon/pkg/camera"
+	"github.com/tauraamui/dragondaemon/pkg/log"
+	"github.com/tauraamui/dragondaemon/pkg/video"
+)
+
+type streamConnProccess struct {
+	ctx      context.Context
+	cancel   context.CancelFunc
+	stopping chan interface{}
+	cam      camera.Connection
+	dest     chan video.Frame
+}
+
+func NewStreamConnProcess(cam camera.Connection, dest chan video.Frame) Process {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &streamConnProccess{
+		ctx: ctx, cancel: cancel, cam: cam, dest: dest, stopping: make(chan interface{}),
+	}
+}
+
+func (proc *streamConnProccess) Setup() {}
+func (proc *streamConnProccess) Start() {
+	go proc.run()
+}
+
+func (proc *streamConnProccess) run() {
+	for {
+		time.Sleep(1 * time.Microsecond)
+		select {
+		case <-proc.ctx.Done():
+			close(proc.stopping)
+			return
+		default:
+			stream(proc.cam, proc.dest)
+		}
+	}
+}
+
+func stream(cam camera.Connection, frames chan video.Frame) {
+	if cam.IsOpen() {
+		log.Debug("Reading frame from vid stream for camera [%s]", cam.Title())
+		frame, err := cam.Read()
+		if err != nil {
+			log.Error(fmt.Errorf("Unable to retrieve frame: %w. Auto re-connecting is not yet implemented", err).Error())
+			return
+		}
+		select {
+		case frames <- frame:
+			log.Debug("Sending frame from cam to buffer...")
+		default:
+			frame.Close()
+			log.Debug("Buffer full...")
+		}
+	}
+}
+
+func (proc *streamConnProccess) Stop() {
+	log.Info("Shutting down stream of camera connection: [%s]", proc.cam.Title())
+	proc.cancel()
+}
+func (proc *streamConnProccess) Wait() {
+	<-proc.stopping
+}
