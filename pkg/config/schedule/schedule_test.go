89039430c33fbcb074ac8e2b37c8803ef666d500
tauraamui <adampstringer@protonmail.com>
2021-10-12 23:25:26 +0100

    Move test helper defs

--- pkg/config/schedule/schedule_test.go
+++ pkg/config/schedule/schedule_test.go
@@ -7,44 +7,6 @@ import (
 	"github.com/matryer/is"
 )
 
-var defaultDate timeDate = timeDate{2021, 9, 13}
-
-func timeFromHoursAndMinutes(td timeDate, hour, minute int) Time {
-	return Time(time.Date(td.year, time.Month(td.month), td.day, hour, minute, 0, 0, time.UTC))
-}
-
-// used in place of unavailable named params langauge feature
-type args struct {
-	date         timeDate
-	hour, minute int
-}
-
-type timeDate struct {
-	year, month, day int
-}
-
-func (td timeDate) empty() bool {
-	if td.year == 0 || td.month == 0 || td.day == 0 {
-		return true
-	}
-	return false
-}
-
-func testTime(a args) Time {
-	date := func() timeDate {
-		if a.date.empty() {
-			return defaultDate
-		}
-		return a.date
-	}()
-	return timeFromHoursAndMinutes(date, a.hour, a.minute)
-}
-
-func testTimePtr(a args) *Time {
-	tt := testTime(a)
-	return &tt
-}
-
 type test struct {
 	skip              bool
 	title             string
@@ -226,6 +188,44 @@ func TestSameDayScheduleTimesMatchExpectedState(t *testing.T) {
 	for _, tt := range tests {
 		runIsTimeOnOrOffTest(t, tt)
 	}
+}
+
+// used in place of unavailable named params langauge feature
+type args struct {
+	date         timeDate
+	hour, minute int
+}
+
+type timeDate struct {
+	year, month, day int
+}
+
+func (td timeDate) empty() bool {
+	if td.year == 0 || td.month == 0 || td.day == 0 {
+		return true
+	}
+	return false
+}
+
+var defaultDate timeDate = timeDate{2021, 9, 13}
+
+func timeFromHoursAndMinutes(td timeDate, hour, minute int) Time {
+	return Time(time.Date(td.year, time.Month(td.month), td.day, hour, minute, 0, 0, time.UTC))
+}
+
+func testTime(a args) Time {
+	date := func() timeDate {
+		if a.date.empty() {
+			return defaultDate
+		}
+		return a.date
+	}()
+	return timeFromHoursAndMinutes(date, a.hour, a.minute)
+}
+
+func testTimePtr(a args) *Time {
+	tt := testTime(a)
+	return &tt
 }
 
 func runIsTimeOnOrOffTest(t *testing.T, tt test) {
