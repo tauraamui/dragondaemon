b7dc953541b0479f2c3517d90406d3d8d7cb7bd0
tauraamui <adampstringer@protonmail.com>
2021-10-22 23:06:31 +0100

    Remove unneeded test from stream conn proc

--- pkg/dragon/process/stream_conn_process_test.go
+++ pkg/dragon/process/stream_conn_process_test.go
@@ -207,60 +207,6 @@ func callWTimeout(f func(), t <-chan time.Time, errmsg string) error {
 	}
 }
 
-// TODO(tauraamui): actually move this test to the schedule package tests
-func (suite *StreamConnProcessTestSuite) TestStreamConnProcessStopsReadingFramesAfterCamTurnsOff() {
-	suite.T().Skip()
-	process.TimeNow = suite.timeNowQuery
-	schedule.TODAY = time.Date(2021, 3, 17, 0, 0, 0, 0, time.UTC)
-	suite.baseTime = time.Date(2021, 3, 17, 13, 0, 0, 0, time.UTC)
-
-	broadcst := broadcast.New(0)
-	listener := broadcst.Listen()
-
-	clipFrameCount := 36
-	frames := make([]mockFrame, clipFrameCount)
-	testConn := mockCameraConn{
-		isOpen: true, framesToRead: frames, schedule: schedule.NewSchedule(schedule.Week{
-			Wednesday: schedule.OnOffTimes{
-				// Off: testTimePtr(args{
-				// 	hour: 13, minute: 0, second: clipFrameCount / 2,
-				// }),
-			},
-		}),
-	}
-
-	// make test channel buffered to allow the send
-	// routine to optionally send, and our test reciever
-	// to optionally recieve without blocking so the loop
-	// proceeds and the timeout is checked
-	readFrames := make(chan video.Frame, 3)
-	proc := process.NewStreamConnProcess(listener, &testConn, readFrames)
-
-	proc.Setup().Start()
-	timeout := time.After(3 * time.Second)
-readFrameProcLoop:
-	for {
-		select {
-		case <-timeout:
-			suite.T().Fatal("test timeout 3s limit exceeded")
-			break readFrameProcLoop
-		case msg := <-listener.Ch:
-			if evt, ok := msg.(process.Event); ok && evt == process.CAM_SWITCHED_OFF_EVT {
-				target := (clipFrameCount / 2) + 1
-				if suite.timeSecondOffset != target {
-					suite.T().Fatal(
-						fmt.Sprintf("camera off sent at wrong time: %ds/%d in", suite.timeSecondOffset, target),
-					)
-				}
-				break readFrameProcLoop
-			}
-		case <-readFrames:
-		}
-	}
-	proc.Stop()
-	proc.Wait()
-}
-
 func (suite *StreamConnProcessTestSuite) TestStreamConnProcessUnableToReturnFrameDueToNoReader() {
 	is := is.New(suite.T())
 
