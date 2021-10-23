4594de1b371225a751b562c41d00d9c6fc8a995b
tauraamui <adampstringer@protonmail.com>
2021-10-23 20:34:25 +0100

    Implement custom type output and include tests

--- internal/xerror/error.go
+++ internal/xerror/error.go
@@ -1,23 +1,34 @@
 package xerror
 
-import "errors"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+)
 
 type I interface {
 	Error() string
 	Msg(string) I
-	WithParams(map[string]string) I
+	WithParam(string, interface{}) I
+	WithParams(map[string]interface{}) I
+	ToString() string
 }
+
+type Kind string
 
 type x struct {
-	err    error
-	params map[string]string
+	kind   Kind
 	errMsg string
+	error  error
+	params map[string]interface{}
 }
 
-func New() I {
-	return &x{
-		err: errors.New(""),
-	}
+func New(k Kind, es string) I {
+	i := x{kind: k, errMsg: es}
+	e := fmt.Errorf("KIND: %s | %s", i.kind, i.errMsg)
+	i.error = errors.WithStack(e)
+	return &i
 }
 
 func (x *x) Error() string {
@@ -29,7 +40,49 @@ func (x *x) Msg(m string) I {
 	return x
 }
 
-func (x *x) WithParams(p map[string]string) I {
-	x.params = p
+// WithParams will set or merge given params map to use
+// as the param values. Passing nil instead of a map will
+// clear the params completely.
+func (x *x) WithParams(p map[string]interface{}) I {
+	if p == nil {
+		x.params = p
+		return x
+	}
+
+	if x.params != nil {
+		mergeParams(x.params, p)
+		return x
+	}
 	return x
+}
+
+func mergeParams(p1, p2 map[string]interface{}) {
+	for k, v := range p2 {
+		p1[k] = v
+	}
+}
+
+// WithParam will append the param key/value pair passed in
+// to the internal map.
+func (x *x) WithParam(key string, v interface{}) I {
+	if x.params == nil {
+		x.params = map[string]interface{}{}
+	}
+	x.params[key] = v
+	return x
+}
+
+func (x *x) ToString() string {
+	logMsg := fmt.Sprintf("Kind: %s, Msg: %s", x.kind, x.errMsg)
+
+	params := []string{}
+	for k, v := range x.params {
+		params = append(params, fmt.Sprintf("%s: {%+v}", strings.ToUpper(k), v))
+	}
+	return fmt.Sprintf("%s%s", logMsg, func() string {
+		if len(params) != 0 {
+			return fmt.Sprintf(", Params: [%+v]", strings.Join(params, " | "))
+		}
+		return ""
+	}())
 }
--- internal/xerror/error_test.go
+++ internal/xerror/error_test.go
@@ -10,8 +10,26 @@ import (
 func TestNewErrorGivesErrInstance(t *testing.T) {
 	is := is.New(t)
 
-	err := xerror.New().Msg("order was declined").WithParams(
-		map[string]string{"cheese": "smelt awful"},
-	)
+	err := xerror.New("", "")
 	is.True(err != nil)
+}
+
+func TestNewErrorGivesErrWhichPrintsOutExpectedString(t *testing.T) {
+	is := is.New(t)
+
+	err := xerror.New("TEST_ERROR", "this was caused by something bad: some other wrapped error")
+	is.True(err != nil)
+
+	is.Equal(err.ToString(), "Kind: TEST_ERROR, Msg: this was caused by something bad: some other wrapped error")
+}
+
+func TestNewErrorWithParamsGivesErrWhichPrintsOutExpectedString(t *testing.T) {
+	is := is.New(t)
+
+	err := xerror.New(
+		"TEST_PARAMS_ERROR", "fake request failed",
+	).WithParam("test-request-trace-id", "31257919-40e6-496b-bb53-b71999222b0b")
+	is.True(err != nil)
+
+	is.Equal(err.ToString(), "Kind: TEST_PARAMS_ERROR, Msg: fake request failed, Params: [TEST-REQUEST-TRACE-ID: {31257919-40e6-496b-bb53-b71999222b0b}]")
 }
