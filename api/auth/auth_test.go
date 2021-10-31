e909dc4d840b1ef9e9c1aa7dd946b15add7683e4
tauraamui <adampstringer@protonmail.com>
2021-10-31 23:02:38 +0000

    Remove old auth tests

--- api/auth/auth_test.go
+++ api/auth/auth_test.go
@@ -44,6 +44,53 @@ func TestGenTokenReturnsSignedJWTContainingPayload(t *testing.T) {
 	is.Equal(time.Unix(int64(tc.ExpiresAt), 0).Minute(), 15)
 }
 
+func TestValidateTokenReturnCurrentUserUUIDFromPayload(t *testing.T) {
+	is := is.New(t)
+
+	token, err := auth.GenToken(testingSecret, "testuser")
+	is.NoErr(err)
+	is.True(len(token) > 0)
+
+	uuid, err := auth.ValidateToken(testingSecret, token)
+	is.NoErr(err)
+	is.Equal(uuid, "testuser")
+}
+
+func TestHandleValidationErrorGracefullyAndReturnWrappedError(t *testing.T) {
+	is := is.New(t)
+
+	token, err := auth.GenToken(testingSecret, "testuser")
+	is.NoErr(err)
+	is.True(len(token) > 0)
+
+	uuid, err := auth.ValidateToken("incorrect-secret", token)
+	is.True(err != nil)
+	is.Equal(err.Error(), "unable to validate token: signature is invalid")
+	is.True(len(uuid) == 0)
+}
+
+func TestHandleUnableToParseClaimsErrorGracefullyAndReturnError(t *testing.T) {
+	is := is.New(t)
+
+	uuid, err := auth.CheckClaims(auth.NotPointerForCustomClaims)
+	is.True(err != nil)
+	is.Equal(err.Error(), "unable to parse claims")
+	is.True(len(uuid) == 0)
+}
+
+func TestHandleTokenExpiredErrorGracefullyAndReturnError(t *testing.T) {
+	is := is.New(t)
+
+	auth.CustomClaims.StandardClaims = jwt.StandardClaims{
+		ExpiresAt: 1,
+	}
+
+	uuid, err := auth.CheckClaims(auth.CustomClaims)
+	is.True(err != nil)
+	is.Equal(err.Error(), "auth token has expired")
+	is.True(len(uuid) == 0)
+}
+
 func overloadTimeNow() func() {
 	return auth.OverloadTimeNow(
 		func() time.Time {
