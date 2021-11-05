ac2130c8404529cc7cdc938233c72d7e2f534ba2
tauraamui <adampstringer@protonmail.com>
2021-11-05 20:49:27 +0000

    Test password comparison failure error

--- pkg/database/models/user_test.go
+++ pkg/database/models/user_test.go
@@ -29,3 +29,16 @@ func TestPopulatedUserBeforeCreateShouldGenerateUUIDAndEncryptAuthHash(t *testin
 	is.True(strings.Contains(user.AuthHash, "$2a$10$"))
 	is.NoErr(user.ComparePassword("test-user-password")) // match enc auth hash to plaintxt password
 }
+
+func TestPopulatedUserBeforeCreateShouldFailToMatchPasswordIfIncorrect(t *testing.T) {
+	is := is.New(t)
+	user := models.User{
+		Name:     "test-user-account",
+		AuthHash: "test-user-password",
+	}
+
+	is.NoErr(user.BeforeCreate(nil))
+	err := user.ComparePassword("incorrect-password")
+	is.True(err != nil)
+	is.Equal(err.Error(), "incorrect password: crypto/bcrypt: hashedPassword is not the hash of the given password") // fail to match enc auth hash to plaintxt password
+}
