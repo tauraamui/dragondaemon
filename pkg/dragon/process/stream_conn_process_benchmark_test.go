be41b678795e3daf2f07b25ca5d53c950b125da8
tauraamui <adampstringer@protonmail.com>
2021-11-10 23:23:41 +0000

    Begin to implement benchmark tests

--- pkg/dragon/process/stream_conn_process_benchmark_test.go
+++ pkg/dragon/process/stream_conn_process_benchmark_test.go
@@ -1,18 +1,33 @@
-package process_test
+package process
 
 import (
-	"context"
 	"testing"
 
-	"github.com/tauraamui/dragondaemon/pkg/camera"
-	"github.com/tauraamui/dragondaemon/pkg/video/videobackend"
+	"github.com/tauraamui/dragondaemon/pkg/mocks"
+	"github.com/tauraamui/dragondaemon/pkg/video/videoframe"
 )
 
-func BenchmarkRunningStreamConnProcess(b *testing.B) {
-	backend := videobackend.Mock()
-	conn, err := backend.Connect(context.Background(), "fake-addr")
-	if err != nil {
-		b.Fatal(err)
+func BenchmarkReadingFramesFromMockBackendConnectionStream(b *testing.B) {
+	dest := make(chan videoframe.NoCloser)
+	stopReads := make(chan struct{})
+	go func(frames <-chan videoframe.NoCloser, stop <-chan struct{}) {
+		for {
+			select {
+			case <-stop:
+				return
+			case <-frames:
+			default:
+				continue
+			}
+		}
+	}(dest, stopReads)
+	conn := mocks.NewCamConn(mocks.Options{UntrackedFrames: true})
+	b.ResetTimer()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		stream(conn, dest)
 	}
-	camera.Connect("fake-cam", "fake-addr", camera.Settings{}, backend)
+	b.StopTimer()
+	close(stopReads)
+	close(dest)
 }
---
+++ pkg/mocks/camera_conn.go
@@ -0,0 +1,129 @@
+package mocks
+
+import (
+	"github.com/tauraamui/dragondaemon/pkg/camera"
+	"github.com/tauraamui/dragondaemon/pkg/config/schedule"
+	"github.com/tauraamui/dragondaemon/pkg/video/videoframe"
+	"github.com/tauraamui/xerror"
+)
+
+type Options struct {
+	UntrackedFrames bool
+}
+
+func NewCamConn(opts Options) camera.Connection {
+	return &mockCameraConn{mockOptions: opts}
+}
+
+type mockFrame struct {
+	data          []byte
+	width, height int
+	isOpen        bool
+	isClosing     bool
+	onClose       func()
+}
+
+func (m *mockFrame) DataRef() interface{} {
+	return m.data
+}
+
+func (m *mockFrame) Dimensions() videoframe.Dimensions {
+	return videoframe.Dimensions{W: m.width, H: m.height}
+}
+
+func (m *mockFrame) Close() {
+	m.isOpen = false
+	m.isClosing = true
+	if m.onClose != nil {
+		m.onClose()
+	}
+}
+
+type mockCameraConn struct {
+	mockOptions         Options
+	uuid                string
+	title               string
+	persistLocation     string
+	fullPersistLocation string
+	maxClipAgeDays      int
+	fps                 int
+	schedule            schedule.Schedule
+	spc                 int
+	frameReadIndex      int
+	framesToRead        []mockFrame
+	readFunc            func() (videoframe.Frame, error)
+	onPostRead          func()
+	readErr             error
+	isOpenFunc          func() bool
+	isOpen              bool
+	isClosing           bool
+	closeErr            error
+}
+
+func (m *mockCameraConn) UUID() string {
+	return m.uuid
+}
+
+func (m *mockCameraConn) Title() string {
+	return m.title
+}
+
+func (m *mockCameraConn) PersistLocation() string {
+	return m.persistLocation
+}
+
+func (m *mockCameraConn) FullPersistLocation() string {
+	return m.fullPersistLocation
+}
+
+func (m *mockCameraConn) MaxClipAgeDays() int {
+	return m.maxClipAgeDays
+}
+
+func (m *mockCameraConn) FPS() int {
+	return m.fps
+}
+
+func (m *mockCameraConn) Schedule() schedule.Schedule {
+	return m.schedule
+}
+
+func (m *mockCameraConn) SPC() int {
+	return m.spc
+}
+
+func (m *mockCameraConn) Read() (frame videoframe.Frame, err error) {
+	if m.onPostRead != nil {
+		defer m.onPostRead()
+	}
+
+	if m.readFunc != nil {
+		return m.readFunc()
+	}
+
+	if m.mockOptions.UntrackedFrames {
+		return &mockFrame{}, nil
+	}
+
+	if m.frameReadIndex+1 >= len(m.framesToRead) {
+		return nil, xerror.New("run out of frames to read")
+	}
+	frame, err = &m.framesToRead[m.frameReadIndex], m.readErr
+	m.frameReadIndex++
+	return
+}
+
+func (m *mockCameraConn) IsOpen() bool {
+	if m.isOpenFunc != nil {
+		return m.isOpenFunc()
+	}
+	return m.isOpen
+}
+
+func (m *mockCameraConn) IsClosing() bool {
+	return m.isClosing
+}
+
+func (m *mockCameraConn) Close() error {
+	return m.closeErr
+}
