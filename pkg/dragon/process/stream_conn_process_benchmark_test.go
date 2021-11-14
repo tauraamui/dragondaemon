a1f04938e5b763c498fea6050e4ba6fe4dbe98c1
tauraamui <adampstringer@protonmail.com>
2021-11-14 16:15:03 +0000

    Benchmark stream proc reading from heavy weight mock conn

--- pkg/dragon/process/stream_conn_process_benchmark_test.go
+++ pkg/dragon/process/stream_conn_process_benchmark_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 
 	"github.com/tauraamui/dragondaemon/pkg/broadcast"
+	"github.com/tauraamui/dragondaemon/pkg/camera"
 	"github.com/tauraamui/dragondaemon/pkg/mocks"
+	"github.com/tauraamui/dragondaemon/pkg/video/videobackend"
 	"github.com/tauraamui/dragondaemon/pkg/video/videoframe"
 )
 
-func BenchmarkReadingFramesFromMockBackendConnectionStream(b *testing.B) {
+func BenchmarkReadingFramesFromInfinateLowSizeFrameProducer(b *testing.B) {
 	dest := make(chan videoframe.NoCloser)
 	stopReads := make(chan struct{})
 	go func(frames <-chan videoframe.NoCloser, stop <-chan struct{}) {
@@ -33,11 +35,43 @@ func BenchmarkReadingFramesFromMockBackendConnectionStream(b *testing.B) {
 	close(dest)
 }
 
-func BenchmarkStreamConnProcessReading10000Frames(b *testing.B) {
+func BenchmarkStreamConnProcessReading10000LowSizeFrames(b *testing.B) {
 	const maxFrames uint = 10000
 
 	readFrames := make(chan videoframe.NoCloser, 3)
 	conn := mocks.NewCamConn(mocks.Options{UntrackedFrames: true, IsOpen: true})
+	proc := NewStreamConnProcess(broadcast.New(0).Listen(), "testCam", conn, readFrames)
+
+	proc.Setup().Start()
+
+	b.ResetTimer()
+	b.StartTimer()
+	var count uint
+procLoop:
+	for {
+		select {
+		case <-readFrames:
+			count++
+		default:
+			if count >= maxFrames {
+				break procLoop
+			}
+		}
+	}
+	b.StopTimer()
+
+	<-proc.Stop()
+}
+
+func BenchmarkStreamConnProcessReading100FramesFromMockVideoStream(b *testing.B) {
+	const maxFrames uint = 100
+
+	readFrames := make(chan videoframe.NoCloser, 3)
+	conn, err := camera.Connect("mock", "", camera.Settings{}, videobackend.Mock())
+	if err != nil {
+		b.Fatal("unable to open mock connection: %w", err)
+	}
+
 	proc := NewStreamConnProcess(broadcast.New(0).Listen(), "testCam", conn, readFrames)
 
 	proc.Setup().Start()
